1a991728f16e7ed5fb9e7131f1b450badf1caa32
Damon Morris <d.j.morris@sheffield.ac.uk>
2023-01-16 15:32:29 +0000

    update data documentation

--- R/data.R
+++ R/data.R
@@ -1,7 +1,11 @@
 #' Aggregate Hours and Earnings Data
 #'
 #' Time series of aggregate data on mean and median hourly earnings (excluding overtime) and weekly paid hours. Also median weekly
-#' and annual earnings. Produced by the Office for National Statistics.
+#' and annual earnings. Produced by the Office for National Statistics using data from the Annual Survey of Hours and Earnings (ASHE), a business
+#' survey data-set conducted in April of each year completed by employers and based on a 1% random sample of the working age population sampled
+#' by National Insurance number.
+#'
+#' Note that annual earnings data are produced from a sub-sample of individuals who were employed continuously for the previous 12 months.
 #'
 #' @source \href{https://www.ons.gov.uk/employmentandlabourmarket/peopleinwork/earningsandworkinghours/datasets/ashe1997to2015selectedestimates}{Annual Survey of Hours and Earnings time series of selected estimates} 26 October 2022 release.
 #'
